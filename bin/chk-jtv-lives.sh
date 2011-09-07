57ef249905fdc76f92d5ee79781822fc5169b804
Yu-Jie Lin <livibetter@gmail.com>
2011-09-07 19:07:50 +0800

    Fix xslt template file path

--- bin/chk-jtv-lives.sh
+++ bin/chk-jtv-lives.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Copyright 2011 Yu-Jie Lin
 # WTFPL License
+XSLT="$(readlink "$0")"
+XSLT="${XSLT%.sh}.xslt"
 
 JTV_USERNAME="${1:-livibetter}"
 LOGINS="$JTV_USERNAME,$(wget -q "http://www.justin.tv/$JTV_USERNAME/following" -O - |
@@ -9,7 +11,7 @@ sed -n '/videos">$/ {s/\([^\/]\+\/\)\([^\/]\+\).*/\2/;H} ; $ {x;s/\n/,/g;p}')"
 i=0
 API_URL="http://api.justin.tv/api/stream/list.xml?channel=$LOGINS"
 wget -q "$API_URL" -O - |
-xsltproc chk-jtv-lives.xslt - |
+xsltproc "$XSLT" - |
 sed $'s/ANSI/\033/g' |
 fold -w 68 |
 sed '/32m/ n ; s/^./    &/'
