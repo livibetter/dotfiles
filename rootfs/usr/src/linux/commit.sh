fb6c0bfeec921ee04feb8449c49cf3f938dd26bd
Yu-Jie Lin <livibetter@gmail.com>
2012-12-20 13:46:17 +0800

    Add script to auto-commit process with pre-defined message

---
+++ rootfs/usr/src/linux/commit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TMP="$(readlink /usr/src/linux)"
+DIR_KERNEL="${TMP%/}"
+
+# I really like to do in bad way, bite me! XD
+eval "$(grep LOCALVERSION= kernel-config)"
+msg="Update kernel .config ${DIR_KERNEL}${CONFIG_LOCALVERSION}"
+
+git add kernel-config
+git commit -m "$msg"
