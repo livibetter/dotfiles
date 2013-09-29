4d8b8a7ee7a40fd4b5a62c5bb822e23a7261171e
Yu-Jie Lin <livibetter@gmail.com>
2013-09-30 06:24:35 +0800

    fix gentoo-sources with revision number causing filename becomes "...-gentoo-gentoo-..."

--- rootfs/usr/src/linux/install-to-boot.sh
+++ rootfs/usr/src/linux/install-to-boot.sh
@@ -5,12 +5,11 @@ DIR_KERNEL="${TMP%/}"
 KC=/usr/src/$DIR_KERNEL/.config
 
 VERSION=${DIR_KERNEL#*-}
-VERSION=${VERSION%-*}
 
 # I really like to do in bad way, bite me! XD
 eval "$(grep LOCALVERSION= "$KC")"
 
-BOOTIMG=/boot/kernel-$(uname -m)-$VERSION-gentoo$CONFIG_LOCALVERSION
+BOOTIMG=/boot/kernel-$(uname -m)-$VERSION$CONFIG_LOCALVERSION
 
 if cp /usr/src/$DIR_KERNEL/arch/$(uname -m)/boot/bzImage "$BOOTIMG"; then
   sed -i "
@@ -18,10 +17,10 @@ if cp /usr/src/$DIR_KERNEL/arch/$(uname -m)/boot/bzImage "$BOOTIMG"; then
 \\
 title Gentoo Linux $VERSION$CONFIG_LOCALVERSION\\
 root (hd0,0)\\
-kernel /boot/kernel-$(uname -m)-$VERSION-gentoo$CONFIG_LOCALVERSION root=/dev/sda3 quiet\\
+kernel /boot/kernel-$(uname -m)-$VERSION$CONFIG_LOCALVERSION root=/dev/sda3 quiet\\
 \\
 title Gentoo Linux $VERSION$CONFIG_LOCALVERSION (recuse)\\
 root (hd0,0)\\
-kernel /boot/kernel-$(uname -m)-$VERSION-gentoo$CONFIG_LOCALVERSION root=/dev/sda3 init=/bin/bb
+kernel /boot/kernel-$(uname -m)-$VERSION$CONFIG_LOCALVERSION root=/dev/sda3 init=/bin/bb
 " /boot/grub/grub.conf
 fi
