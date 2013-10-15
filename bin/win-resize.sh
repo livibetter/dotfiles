1f9a6a662315d2c286dc6e9b1cbed2117848c0db
Yu-Jie Lin <livibetter@gmail.com>
2013-10-15 13:28:43 +0800

    use ":SELECT:" instead of parsing xwininfo output

--- bin/win-resize.sh
+++ bin/win-resize.sh
@@ -3,10 +3,9 @@
 # Written by Yu-Jie Lin
 # Public Domain
 
-WIN_ID=$(xwininfo | egrep -om 1 '0x[0-9a-z]+')
 W=$1
 H=$2
 X=${3:--1}
 Y=${4:--1}
 
-wmctrl -i -r $WIN_ID -e 0,$X,$Y,$W,$H
+wmctrl -r :SELECT: -e 0,$X,$Y,$W,$H
