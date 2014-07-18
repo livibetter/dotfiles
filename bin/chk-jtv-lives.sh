336b1395c0ecac8effe1b09edc862285ef7c1d57
Yu-Jie Lin <livibetter@gmail.com>
2014-07-18 10:26:31 +0800

    update new chk-jtv-lives.sh and chk-jtv-lives.xslt location at https://gist.github.com/livibetter/614a080fd08e05d86bb6

--- bin/chk-jtv-lives.sh
+++ bin/chk-jtv-lives.sh
@@ -1,41 +1,4 @@
 #!/bin/bash
-# Copyright 2011, 2012 Yu-Jie Lin
-# WTFPL License
-#
-# Checks Justin.tv user's favored channels live status
-#
-# Usage
-#   chk-jtv-lives.sh [USERNAME]
-#
-# USERNAME default is $USER
-#
-# Requires
-#   td.sh <http://code.google.com/p/yjl/source/browse/Bash/td.sh>
-#   xsltproc
-#   wget
-
-XSLT="$(readlink "$0")"
-XSLT="${XSLT%.sh}.xslt"
-
-JTV_USERNAME="${1:-$USER}"
-# Remove http://..../ part
-JTV_USERNAME="${JTV_USERNAME##*/}"
-# Favorites are whom this user follows
-# http://apiwiki.justin.tv/mediawiki/index.php/User/favorites
-LOGINS="$JTV_USERNAME$(wget -q "http://api.justin.tv/api/user/favorites/$JTV_USERNAME.xml?live=true" -O - |
-sed -n '/login/ {s/ \+<login>\([^<]\+\)<\/login>/\1/;H} ; $ {x;s/\n/,/g;p}')"
-
-i=0
-API_URL="http://api.justin.tv/api/stream/list.xml?channel=$LOGINS"
-wget -q "$API_URL" -O - |
-xsltproc "$XSLT" - |
-sed $'s/ANSI/\033/g' |
-while read line; do
-  if [[ "$line" =~ "%DATE%" ]]; then
-    echo "NOFOLD$(td.sh $(($(date +%s) - $(TZ=US/Pacific date -d "${line:6}" +%s)))) ago"
-  else
-    echo "$line"
-  fi
-done |
-fold -w 68 -s |
-sed '/NOFOLD/ {s/NOFOLD//;p;d} ; s/^./    &/'
+echo 'This script has been moved to
+  https://gist.github.com/livibetter/614a080fd08e05d86bb6' >&2
+exit 1
--- bin/chk-jtv-lives.xslt
+++ bin/chk-jtv-lives.xslt
@@ -1,39 +1,9 @@
 <xsl:stylesheet version="1.0"
 xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-<xsl:output method="text" disable-output-escaping="yes"/>
 
 <xsl:template match="/">
-  <xsl:for-each select="*/stream">
-    <xsl:text>NOFOLDANSI[1;32m</xsl:text>
-      <xsl:value-of select="channel/title"/>
-    <xsl:text>ANSI[0m</xsl:text>
-
-    <xsl:text> </xsl:text>
-      <xsl:value-of select="channel_count"/> 
-    <xsl:text> (</xsl:text>
-      <xsl:value-of select="embed_count"/>
-    <xsl:text> embeds)&#xA;</xsl:text>
-    
-    <xsl:text>NOFOLD</xsl:text>
-    <xsl:value-of select="channel/channel_url"/>
-    <xsl:text>&#xA;</xsl:text>
-
-    <xsl:text>%DATE%</xsl:text>
-    <xsl:value-of select="up_time"/>
-    <xsl:text>&#xA;&#xA;</xsl:text>
-
-    <xsl:text>ANSI[1;37m</xsl:text>
-    <xsl:choose>
-      <xsl:when test="channel/status != ''">
-        <xsl:value-of select="channel/status"/>
-      </xsl:when>
-      <xsl:otherwise>
-        <xsl:text>[no status]</xsl:text>
-      </xsl:otherwise>
-    </xsl:choose>
-    <xsl:text>ANSI[0m</xsl:text>
-    <xsl:text>&#xA;&#xA;</xsl:text>
-  </xsl:for-each>
+This XSLT has been moved to
+  https://gist.github.com/livibetter/614a080fd08e05d86bb6
 </xsl:template>
 
 </xsl:stylesheet>
