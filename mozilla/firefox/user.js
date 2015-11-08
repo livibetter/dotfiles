7869848b0f329f05665e4e124a89df30d630fdbb
Yu-Jie Lin <livibetter@gmail.com>
2015-11-09 06:16:07 +0800

    remove unused Firefox files

--- mozilla/firefox/user.js
+++
@@ -1,5 +0,0 @@
-//user_pref("capability.policy.policynames", "NoDamnFocusThieves");
-//user_pref("capability.policy.NoDamnFocusThieves.sites", "http://www.youtube.com http://www.google.com");
-// Won't work on Google Search, only domain level is valid
-//user_pref("capability.policy.NoDamnFocusThieves.sites", "http://www.google.com/search");
-//user_pref("capability.policy.NoDamnFocusThieves.HTMLInputElement.focus", "noAccess");
