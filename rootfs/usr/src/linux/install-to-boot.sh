34d70dc32d55912c8ccdeba496056ca7f9099cf8
Yu-Jie Lin <livibetter@gmail.com>
2022-12-25 16:02:26 +0800

    remove intel-ucode

--- rootfs/usr/src/linux/install-to-boot.sh
+++ rootfs/usr/src/linux/install-to-boot.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-TMP="$(readlink /usr/src/linux)"
-DIR_KERNEL="${TMP%/}"
-KC=/usr/src/$DIR_KERNEL/.config
-
-VERSION=${DIR_KERNEL#*-}
-
-# I really like to do in bad way, bite me! XD
-eval "$(grep LOCALVERSION= "$KC")"
-
-BOOTIMG=/boot/kernel-$(uname -m)-$VERSION$CONFIG_LOCALVERSION
-UCODE=/lib/firmware/intel-ucode
-
-if cp /usr/src/$DIR_KERNEL/arch/$(uname -m)/boot/bzImage "$BOOTIMG" \
-&& iucode_tool --overwrite -S --write-earlyfw=/boot/early_ucode.cpio "$UCODE"/*
-then
-  grub-mkconfig -o /boot/grub/grub.cfg
-fi
+grub-mkconfig -o /boot/grub/grub.cfg
